0d7ed733080dbc86d32a093371c5a059b4dfbe43
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-26 17:26:06 +0200

    jumbotron completato

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,9 @@
 let mix = require('laravel-mix');
 mix
     .js('src/app.js', 'dist')
-    .sass('src/style.scss', '')
+    .sass('src/style.scss','').options({
+        processCssUrls: false
+    })
     .setPublicPath('dist')
     .copy('src/index.html', 'dist')
 ;
