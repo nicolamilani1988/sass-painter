c23c6776c1581fcb2f2cfb0daca6d080e6ca7223
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-26 14:02:41 +0200

    creato file iniziale

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+// webpack.mix.js
+let mix = require('laravel-mix');
+mix
+    .js('src/app.js', 'dist')
+    .sass('src/style.scss', '')
+    .setPublicPath('dist')
+    .copy('src/index.html', 'dist')
+;
